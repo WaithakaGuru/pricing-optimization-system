5e276826c01348467f3424e51e6a304961eeec69
WaithakaGuru <waithakaoffices@gmail.com>
2026-03-17 12:52:27 +0300

    set up tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <section id="center">
+      <section id="center" className="text-blue-400">
         This is OPTIMA your Product Pricing Optimizer
       </section>
     </>
